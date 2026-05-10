d1508f522ee39ff3d60a622728aa3ad8407cf67f
vitaliiantoniuk <vitalii@2solar.nl>
2026-05-10 18:54:48 +0200

    feat: add AiConversationResource and AiMessageResource

---
+++ app/Http/Resources/Api/V1/AiConversationResource.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Resources\Api\V1;
+
+use App\Models\AiConversation;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/** @property AiConversation $resource */
+class AiConversationResource extends JsonResource
+{
+    /** @return array<string, mixed> */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->resource->public_id,
+            'selected_text' => $this->resource->selected_text,
+            'selection_start' => $this->resource->selection_start,
+            'selection_end' => $this->resource->selection_end,
+            'is_private' => $this->resource->is_private,
+            'owner_id' => $this->resource->user_id,
+            'messages' => AiMessageResource::collection(
+                $this->whenLoaded('messages', $this->resource->messages)
+            ),
+            'created_at' => $this->resource->created_at,
+        ];
+    }
+}
